1c37adf5ef081bf913c2f053c51c9b84c079be65
codingtamgu <92910852+codingtamgu@users.noreply.github.com>
2023-04-22 21:16:16 +0900

    Update moms.py

--- moms.py
+++ moms.py
@@ -19,7 +19,7 @@ for i in idlist:
     print(idlist[i])
     print(pwlist[i])
     chrome_options = Options()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
